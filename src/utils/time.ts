319ba9015aa9087c7e746b83169fde562de40b1c
Bruno Braga <brunobragaw8t@gmail.com>
2025-10-26 22:05:01 +0000

    fix(utils): remove hours of duration format when it's zero

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,11 @@
 export function formatDuration(duration: number): string {
   const hours = Math.floor(duration / 60)
   const minutes = duration % 60
+
+  if (hours === 0) {
+    return `${minutes}m`
+  }
+
   return `${hours}h ${minutes}m`
 }
 
